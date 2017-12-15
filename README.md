10e06cf5292faf8fabdc752c6e1f021ae07c59d8
Yan Virin <yvirin@apixio.com>
2017-12-15 11:51:12 -0800

    started to populate README, add comments and parse args

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# material-project
-A summarization project, Columbia University
+# A summarization project, Columbia University
+
+## How to compute baselines?
+
+In the folder training you will find the baseline.py script.
+
+usage: baseline.py [-h] ds ver out mrge typ
+
+positional arguments:
+  ds          the path to the dataset top level folder
+  ver         ROUGE ver to user either 1 or 2 for choosing the best source to
+              use (mds)
+  out         the path of the output ROUGE file; should be global path
+  mrge        the minimal ROUGE2 in the .rge file to consider as valid
+  typ         the type of baseline to run: centroid, first3, rand3
+
+example:
+
+python baseline.py ../../material-data/dataset 1 ../training/baseline.rand3 0.1 rand3
+
 
--- training/baseline.py
+++ training/baseline.py
@@ -1,3 +1,4 @@
+import argparse
 import os, sys
 sys.path.append("../rouge-scripts")
 import rouge as rge
@@ -5,22 +6,13 @@ import utils
 import random
 import math
 
-rouge_dir = "../rouge"
-
-dataset = sys.argv[1]
-ver = sys.argv[2]
-eval_out = sys.argv[3]
-min_rge = float(sys.argv[4])
-typ = sys.argv[5]
+'''
+This script runs the baseline evaluation on the dataset
+It can run the CentroidEmd, First3 (Lead Three) or random baselines
+The output is a ROUGE script output which includes ROUGE1 and ROUGE2 numbers
+'''
 
 FIRST_N_LINES = 3
-
-os.system("mkdir -p /tmp/mds")
-
-eval_path = "/tmp/mds/baseline.cfg"
-eval_writer = open(eval_path, "w")
-
-cache = dict()
 
 def centroidemd(source_path):
   get_embds = lambda path: [[float(y) for y in x.split(" ")] for x in utils.fileaslist(path[:-3]+"emd")]
@@ -48,40 +40,66 @@ def rand3(text_path):
 def first3(text_path):
   return "\n".join(utils.fileaslist(text_path)[:FIRST_N_LINES])
 
-candidate = centroidemd if typ == "centroid" else rand3
 
-print "using %s for candidates" % candidate
+if __name__ == "__main__":
 
-for dp in os.listdir(dataset):
-  if dp == ".README": continue
-  sources = os.path.join(dataset, dp, "sources")
-  best = None
-  max_score = 0.0
-  for s in os.listdir(sources):
-    if s.endswith(".rge"):
-      score = rge.parse_rouge(os.path.join(sources, s), int(ver))
-      if score > max_score:
-        max_score = score
-        best = s
-  if best:
-    base = ".".join(best.split(".")[:-1])
-    s = base+".best"+ver
-    text_path = os.path.join(sources, base+".nrm")
-    cont_path = os.path.join(dataset, dp, "content.txt.nrm")
-    if os.path.exists(os.path.join(sources, s)) and os.path.exists(text_path):
-      if rge.parse_rouge(os.path.join(sources, base+".rge"), 2) < min_rge: continue
-      can_text = candidate(text_path)
-      ref_text = "\n".join([x for x in utils.fileaslist(cont_path) if len(x.split(" "))>3][:FIRST_N_LINES])
-      can_path = "/tmp/mds/%s.can.txt" % base
-      ref_path = "/tmp/mds/%s.ref.txt" % base
-      utils.write2file(can_text, can_path)
-      utils.write2file(ref_text, ref_path)
-      eval_writer.write("%s %s\n" % (can_path, ref_path))
+  parser = argparse.ArgumentParser(description = 'Compute baselines')
+  parser.add_argument('dataset', metavar="ds", help='the path to the dataset top level folder')
+  parser.add_argument('ver', type=int, help='ROUGE ver to user either 1 or 2 for choosing the best source to use (mds)')
+  parser.add_argument('eval_out', metavar="out", help='the path of the output ROUGE file; should be global path')
+  parser.add_argument('min_rge', metavar="mrge", type=float, help='the minimal ROUGE2 in the .rge file to consider as valid')
+  parser.add_argument('typ', help='the type of baseline to run: centroid, first3, rand3')
+  args = parser.parse_args()  
 
-eval_writer.close()
-print "created the evaluation file, running rouge..."
+  dataset = args.dataset
+  ver = str(args.ver)
+  eval_out = args.eval_out
+  min_rge = args.min_rge
+  typ = args.typ
 
-os.chdir(rouge_dir)
-rge.rouge(1000, eval_path, eval_out)
+  rouge_dir = "../rouge"
 
-print "done."
+  os.system("mkdir -p /tmp/mds")
+
+  eval_path = "/tmp/mds/baseline.cfg"
+  eval_writer = open(eval_path, "w")
+
+  cache = dict()
+
+  candidate = centroidemd if typ == "centroid" else (first3 if typ == "first3" else rand3)
+
+  print "using %s for candidates" % candidate
+
+  for dp in os.listdir(dataset):
+    if dp == ".README": continue
+    sources = os.path.join(dataset, dp, "sources")
+    best = None
+    max_score = 0.0
+    for s in os.listdir(sources):
+      if s.endswith(".rge"):
+        score = rge.parse_rouge(os.path.join(sources, s), int(ver))
+        if score > max_score:
+          max_score = score
+          best = s
+    if best:
+      base = ".".join(best.split(".")[:-1])
+      s = base+".best"+ver
+      text_path = os.path.join(sources, base+".nrm")
+      cont_path = os.path.join(dataset, dp, "content.txt.nrm")
+      if os.path.exists(os.path.join(sources, s)) and os.path.exists(text_path):
+        if rge.parse_rouge(os.path.join(sources, base+".rge"), 2) < min_rge: continue
+        can_text = candidate(text_path)
+        ref_text = "\n".join([x for x in utils.fileaslist(cont_path) if len(x.split(" "))>3][:FIRST_N_LINES])
+        can_path = "/tmp/mds/%s.can.txt" % base
+        ref_path = "/tmp/mds/%s.ref.txt" % base
+        utils.write2file(can_text, can_path)
+        utils.write2file(ref_text, ref_path)
+        eval_writer.write("%s %s\n" % (can_path, ref_path))
+
+  eval_writer.close()
+  print "created the evaluation file, running rouge..."
+
+  os.chdir(rouge_dir)
+  rge.rouge(1000, eval_path, eval_out)
+
+  print "done."
